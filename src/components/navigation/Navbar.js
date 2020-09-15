09f98ff0a97fb5496db012ffbf797bda55f82fc1
Katell Goaer <alt.katellgoaer@gmail.com>
2020-09-15 16:59:15 -0400

    styled navbar

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -8,7 +8,7 @@ const StyledNav = styled.nav`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin: 50px 0;
+  margin: 30px 0;
 `
 
 const StyledUl = styled.ul`
@@ -16,10 +16,21 @@ const StyledUl = styled.ul`
   display: flex;
   margin: 0 20px;
   padding: 0;
+  height: 16px;
+  width: 50%;
+
+  & > * {
+    margin: 0 15px;
+  }
+
+  &:last-of-type {
+    justify-content: flex-end;
+    margin-right: 20px;
+  }
 `
 
 const StyledLogo = styled.img`
-  height: 25px;
+  height: 40px;
   width: auto;
 `
 
