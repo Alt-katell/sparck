7dfdf3c6a3cfcb4af5f17de26dfe8d27e190dda5
Katell Goaer <alt.katellgoaer@gmail.com>
2020-09-21 17:22:55 -0400

    styled contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,35 +4,64 @@ import styled from 'styled-components';
 import { usePageContext } from '../context/pageContext';
 
 const StyledForm = styled.form`
-  width: 400px;
-  margin: 100px auto;
+  width: 350px;
+  margin: 130px auto 60px auto;
 `
 
 const StyledItem = styled.div`
   display: flex;
   flex-direction: column;
+  margin-bottom: 28px;
 `
 
 const StyledNameGroup = styled.div`
   display: flex;
   justify-content: space-between;
 
+  & > * {
+    width: 40%;
+  }
 `
 
 const StyledLabel = styled.label`
-
+  margin-bottom: 7px;
 `
 
 const StyledInput = styled.input`
+  border: none;
+  height: 25px;
+  margin-bottom: 9px;
+  font-family: ${props => props.theme.fonts.roboto};
+  font-weight: 300;
+  font-size: 12px;
+`
 
+const StyledTextarea = styled.textarea`
+  border: none;
+  margin-bottom: 9px;
+  font-family: ${props => props.theme.fonts.roboto};
+  font-weight: 300;
+  font-size: 12px;
 `
 
 const StyledUnderline = styled.div`
-
+  width: 100%;
+  border-bottom: 2px solid ${props => props.theme.colors.white};
 `
 
 const StyledSendButton = styled.button`
+  width: 100%;
+  height: 35px;
+  background: ${props => props.theme.colors.black};
+  color: ${props => props.theme.colors.white};
+  border: none;
+  font-family: ${props => props.theme.fonts.roboto};
+  font-weight: 300;
+  cursor: pointer;
 
+  &:hover {
+    font-weight: 700;
+  }
 `
 
 const ContactForm = () => {
@@ -64,7 +93,7 @@ const ContactForm = () => {
 
       <StyledItem>
         <StyledLabel htmlFor="message">{t.message[currentLang]}</StyledLabel>
-        <StyledInput type="textarea" name="message" />
+        <StyledTextarea name="message" rows="7"/>
         <StyledUnderline />
       </StyledItem>
 
