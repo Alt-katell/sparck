0fcfd377e8678011921c40d760ae7d4634cc9069
Katell Goaer <alt.katellgoaer@gmail.com>
2020-09-24 17:16:51 -0400

    changed role for team members in French

--- src/pages/team.fr.js
+++ src/pages/team.fr.js
@@ -25,8 +25,8 @@ const TeamPage = () => {
         reverse={false}
         photo="/images/Photo-Vicky.jpg"
         name="Clémence Paiement"
-        role1="Full-Stack Developer"
-        role2="UX Designer"
+        role1="Développeuse Full-Stack"
+        role2="Designer UX"
         description1="Lorem ipsum dolor sit amet"
         description2="bla bla bla bla bla bla bla Lorem ipsum dolor sit amet, bla bla bla bla bla bla bla Lorem ipsum dolor sit amet, bla bla bla bla bla bla bla Lorem ipsum dolor sit amet, bla bla bla bla bla bla bla Lorem ipsum dolor sit amet"
         description3="bla bla bla bla bla bla bla Lorem ipsum dolor sit amet, bla bla bla bla bla bla bla Lorem ipsum dolor sit amet"
@@ -36,8 +36,8 @@ const TeamPage = () => {
         reverse={true}
         photo="/images/Photo-Katell.png"
         name="Nour Souid"
-        role1="Full-Stack Developer"
-        role2="Business Strategist"
+        role1="Développeuse Full-Stack"
+        role2="Stratège Affaires"
         description1="Lorem ipsum dolor sit amet"
         description2="bla bla bla bla bla bla bla Lorem ipsum dolor sit amet, bla bla bla bla bla bla bla Lorem ipsum dolor sit amet, bla bla bla bla bla bla bla Lorem ipsum dolor sit amet, bla bla bla bla bla bla bla Lorem ipsum dolor sit amet"
         description3="bla bla bla bla bla bla bla Lorem ipsum dolor sit amet, bla bla bla bla bla bla bla Lorem ipsum dolor sit amet"
@@ -47,8 +47,8 @@ const TeamPage = () => {
         reverse={false}
         photo="/images/Photo-Katell.png"
         name="Katell Goaër"
-        role1="Front-End Developer"
-        role2="Brand Builder"
+        role1="Développeuse Front-End"
+        role2="Stratège Marque"
         description1="Katell est une développeuse web orientée projet."
         description2="Tout ce qu’elle fait est motivé par l’empathie. Elle est capable de se mettre à la place de n’importe qui. Pouvoir observer un projet du point de vue d’un designer, d’un techy, de vous ou mieux, de votre client, lui donne une perspective globale de la stratégie."
         description3="Katell a travaillé pendant plus de 5 ans dans le Marketing, à construire des produits, marques et packagings, avant de pivoter vers le web."
@@ -58,8 +58,8 @@ const TeamPage = () => {
         reverse={true}
         photo="/images/Photo-Vicky.jpg"
         name="Vicky André"
-        role1="Front-End Developer"
-        role2="Web Designer"
+        role1="Développeuse Front-End"
+        role2="Designer Web"
         description1="Vicky est une designer multidisciplinaire passionnée."
         description2="Du Design Textile à la conception Web, elle croit que chaque élément graphique se doit d’être utile et réfléchi. Elle imagine des sites Web pour créer des univers uniques et mettre en valeur chaque produit, avec une approche minimaliste et moderne."
         description3="Enseignante et chef d'entreprise, Vicky a le sens du détail et est avide de défis créatifs et mémorables."
