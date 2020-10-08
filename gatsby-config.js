5401eac96153d25e71a97f40dcd901cbd7100ad7
Katell Goaer <alt.katellgoaer@gmail.com>
2020-10-08 18:33:49 -0400

    changed path to social image

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `spärck`,
     description: `spärck is a collaborative web agency. We connect talents to make the world wide web a better and more beautiful place.`,
-    image: `static/images/sparck-screenshot.jpg`,
+    image: `https://sparck.ca/images/sparck-screenshot.jpg`,
     siteUrl: `https://www.sparck.ca`,
   },
   plugins: [
